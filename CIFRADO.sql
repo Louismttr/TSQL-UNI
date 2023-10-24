161418f9041dc7d8c83c3a8ec1c539748892a9b2
Luisa <110619581+Louismttr@users.noreply.github.com>
2023-10-23 18:31:03 -0600

    CIFRADO-INFO

---
+++ CIFRADO.sql
@@ -0,0 +1,24 @@
+--Cifrado 
+-- 1. Seguridad(triaga):
+--- 1.1. Integridad
+--- 1.2. Confidencial
+---- 1.1. y 1.2. Gestión de cuentas y usuario; Cifrado (antiquisido)
+--- 1.3. Disponiblidad: Espejo(baja), Réplica(se necesita un espejo primero) y Cluster(alta)
+--- **1.4** No repudio
+
+--- -.Cifrado: AES, DES, 3DS, IDEA, ---Cifrado de Feistel(Lucifer)...--- ESTUDIAR
+--- -.Algoritmo: simetrico y asimentrico; bit y bloques...
+--- -.Protocolo:
+--{Nota: Texto en claro M(x)=(Todos lo leen)}
+--{Nota: Criptograma C(x)=(Yxpb ab Axqlp)}
+
+
+----- TSQL:
+---- °Encryptbypassphrase
+---- °Decryptbypassphrase
+---- °PwEncrypt
+---- °PwCompare
+
+-- (Los algoritmos mas usados son, MD5 Y funciones Hash) NO USAR
+
+--- RECOMENDACION: Cifrar y volver a cifrar
